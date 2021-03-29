074c9f5fec7b412387efad570ee53f898acdb5ef
saiemgilani <saiem.gilani@gmail.com>
2021-03-29 14:09:56 -0400

    player image

--- src/components/PlayerImage/index.js
+++ src/components/PlayerImage/index.js
@@ -5,7 +5,7 @@ import {Img} from './style';
 
 const PlayerImage = ({player}) => {
   const endpoint = new URL(apiOrigin);
-  endpoint.pathname = `/data/images/players/${player}.png`;
+  endpoint.pathname = `/data/images/Players/${player}.png`;
 
   return <Img src={endpoint.href} alt="Player's portrait" width={180}  mode='fit' />;
 };
